59a1f754b18a7c0641efd7bca7a8897b76b54d8e
Jefferson Duarte <duartejefferson123@gmail.com>
2025-07-07 13:43:02 -0300

    initial commit

---
+++ src/test/java/com/jefferson/vidaplus/VidaplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jefferson.vidaplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidaplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
